d5b5ad9215e0e078923e71e8dc8964b6e409931a
Kevin Savage <kevinpetersavage@users.noreply.github.com>
2018-07-16 14:09:53 +0100

    Fix index bug

--- pyark/subclients/transactions_client.py
+++ pyark/subclients/transactions_client.py
@@ -1,4 +1,5 @@
 from pyark import cva_client
+from pyark.errors import CvaClientError
 
 
 class TransactionsClient(cva_client.CvaClient):
@@ -6,16 +7,27 @@ class TransactionsClient(cva_client.CvaClient):
         cva_client.CvaClient.__init__(self, url_base, token=token)
 
     def get_transaction(self, transaction_id, just_return_status=False):
-        result = self.get("transactions/{identifier}".format(identifier=transaction_id))
+        results = self.get("transactions/{identifier}".format(identifier=transaction_id))
         if just_return_status:
-            return result[0][0]['status']
-        return result
+            return self._first_or_raise([r['status'] for s in results for r in s], transaction_id)
+        else:
+            return results
 
     def retry_transaction(self, transaction_id, just_return_status=False):
-        result = self.patch("transactions/{identifier}".format(identifier=transaction_id))
+        results = self.patch("transactions/{identifier}".format(identifier=transaction_id))
         if just_return_status:
-            return result[0]['response'][0]['result'][0]['status']
-        return result
+            return self._first_or_raise(
+                [t['status'] for r in results for s in r['response'] for t in s['result']],
+                transaction_id
+            )
+        return results
 
     def get_transactions(self, params={}):
         return self.get("transactions", params=params)
+
+    @staticmethod
+    def _first_or_raise(items, id):
+        if items:
+            return items[0]
+        else:
+            raise CvaClientError("no transaction for {}".format(id))
