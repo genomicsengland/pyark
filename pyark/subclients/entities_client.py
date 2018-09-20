a13a571134077e2343eec7a2f0b2f8ea95958b75
priesgo <priesgoferreiro@gmail.com>
2018-09-20 08:26:48 +0100

    added new endpoint for disorders

--- pyark/subclients/entities_client.py
+++ pyark/subclients/entities_client.py
@@ -1,9 +1,7 @@
 import pyark.cva_client as cva_client
 
 
-class PanelsClient(cva_client.CvaClient):
-
-    _BASE_ENDPOINT = "panels"
+class EntitiesClient(cva_client.CvaClient):
 
     def __init__(self, url_base, token):
         cva_client.CvaClient.__init__(self, url_base, token=token)
@@ -16,7 +14,7 @@ class PanelsClient(cva_client.CvaClient):
         :rtype: list
         """
         params = {'use_versions': use_versions}
-        results, _ = self._get("{endpoint}/summary".format(endpoint=self._BASE_ENDPOINT), params=params)
+        results, _ = self._get("panels/summary", params=params)
         return self._render(results, as_data_frame)
 
     def get_all_panels(self):
@@ -26,3 +24,36 @@ class PanelsClient(cva_client.CvaClient):
         """
         results = self.get_panels_summary(use_versions=False)
         return [x['panel']['name'] for x in results]
+
+    def get_disorders_summary(self, as_data_frame=False):
+        """
+        :param as_data_frame: bool
+        :return:
+        :rtype: list
+        """
+        results, _ = self._get("disorders/summary")
+        return self._render(results, as_data_frame)
+
+    def get_all_specific_diseases(self):
+        """
+        :return:
+        :rtype: list
+        """
+        results = self.get_disorders_summary()
+        return [x['disorder']['specificDisease'] for x in results]
+
+    def get_all_disease_groups(self):
+        """
+        :return:
+        :rtype: list
+        """
+        results = self.get_disorders_summary()
+        return [x['disorder']['diseaseGroup'] for x in results]
+
+    def get_all_disease_subgroups(self):
+        """
+        :return:
+        :rtype: list
+        """
+        results = self.get_disorders_summary()
+        return [x['disorder']['diseaseSubGroup'] for x in results]
