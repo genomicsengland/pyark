3b82e0ad9b8bc6fac7f2ab276483b04464ceb846
kevinpetersavage <kevinpetersavage@users.noreply.github.com>
2018-12-20 12:27:25 +0000

    Make the evidences endpoint return evidences

--- pyark/subclients/evidences_client.py
+++ pyark/subclients/evidences_client.py
@@ -9,9 +9,10 @@ class EvidencesClient(cva_client.CvaClient):
     def __init__(self, url_base, token):
         cva_client.CvaClient.__init__(self, url_base, token=token)
 
-    def get_evidences(self, source, as_data_frame=False, **params):
+    def get_evidences(self, source, **params):
         url = "{endpoint}/sources/{source}".format(endpoint=self._BASE_ENDPOINT, source=source)
-        return self._paginate(endpoint=url, as_data_frame=as_data_frame, **params)
+        results = self._paginate(endpoint=url, **params)
+        return map(lambda x: EvidenceEntryAndVariants.fromJsonDict(x), results)
 
     def post_evidences(self, evidence, **params):
         """
