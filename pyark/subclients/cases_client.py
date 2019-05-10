3f8aeef9125f5aa856cbea17738c4ec809987c36
priesgo <priesgoferreiro@gmail.com>
2019-05-10 07:06:19 +0100

    upgrade references to old models

--- pyark/subclients/cases_client.py
+++ pyark/subclients/cases_client.py
@@ -1,5 +1,5 @@
 import pyark.cva_client as cva_client
-from protocols.protocol_7_0.cva import Program, Assembly, ReportEventType
+from protocols.protocol_7_2.cva import Program, Assembly, ReportEventType
 import logging
 from enum import Enum
 
